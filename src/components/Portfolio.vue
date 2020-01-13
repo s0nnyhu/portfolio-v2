79b0abf6abe86dc9706dc43c937d80a9ff472ebf
Sonny hu <husonny1@gmail.com>
2020-01-13 18:53:20 +0100

    Keyevent added

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,37 +1,36 @@
 <template>
-	<div id="portfolio">
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quas nihil
-		magnam quos itaque deleniti tempore omnis, sapiente adipisci veritatis
-		architecto recusandae id mollitia eaque molestias voluptates, doloribus quis
-		maxime excepturi cupiditate accusamus blanditiis. Suscipit id sint numquam
-		excepturi? Ab exercitationem aliquid perferendis facere aliquam debitis vero
-		voluptatem labore, molestias eligendi magni accusamus asperiores unde
-		consequatur excepturi ducimus deleniti sed cum itaque aperiam, atque tempore
-		quam harum voluptate. Amet vel corporis voluptas eaque quibusdam odit, nihil
-		voluptatum ea corrupti, provident et velit suscipit totam enim adipisci
-		vitae sint at, laudantium officiis maiores perspiciatis! Blanditiis,
-		reiciendis. Eos soluta minus reiciendis quos dignissimos amet nobis
-		quibusdam illum asperiores odit corporis possimus repellendus dicta optio,
-		vitae similique dolore eaque repudiandae rerum quaerat a, qui blanditiis ex
-		accusamus. Consequuntur consectetur inventore voluptas doloremque. Dolores
-		suscipit optio, expedita repellendus dolorem aspernatur nulla, totam, ipsa
-		cum odit eum laudantium? Debitis quas reprehenderit quisquam eaque. Minus
-		ipsum impedit dolore necessitatibus cum eaque maiores veritatis quis id
-		excepturi beatae nisi reiciendis dignissimos recusandae dicta nam tempore
-		eius nobis, vitae earum porro repudiandae quod accusantium. Non fugit
-		inventore ea?
-	</div>
+  <div id="portfolio">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quas nihil
+    magnam quos itaque deleniti tempore omnis, sapiente adipisci veritatis
+    architecto recusandae id mollitia eaque molestias voluptates, doloribus quis
+    maxime excepturi cupiditate accusamus blanditiis. Suscipit id sint numquam
+    excepturi? Ab exercitationem aliquid perferendis facere aliquam debitis vero
+    voluptatem labore, molestias eligendi magni accusamus asperiores unde
+    consequatur excepturi ducimus deleniti sed cum itaque aperiam, atque tempore
+    quam harum voluptate. Amet vel corporis voluptas eaque quibusdam odit, nihil
+    voluptatum ea corrupti, provident et velit suscipit totam enim adipisci
+    vitae sint at, laudantium officiis maiores perspiciatis! Blanditiis,
+    reiciendis. Eos soluta minus reiciendis quos dignissimos amet nobis
+    quibusdam illum asperiores odit corporis possimus repellendus dicta optio,
+    vitae similique dolore eaque repudiandae rerum quaerat a, qui blanditiis ex
+    accusamus. Consequuntur consectetur inventore voluptas doloremque. Dolores
+    suscipit optio, expedita repellendus dolorem aspernatur nulla, totam, ipsa
+    cum odit eum laudantium? Debitis quas reprehenderit quisquam eaque. Minus
+    ipsum impedit dolore necessitatibus cum eaque maiores veritatis quis id
+    excepturi beatae nisi reiciendis dignissimos recusandae dicta nam tempore
+    eius nobis, vitae earum porro repudiandae quod accusantium. Non fugit
+    inventore ea?
+  </div>
 </template>
 
 <script>
 export default {
-	name: 'Portfolio'
+  name: "Portfolio"
 };
 </script>
 
 <style>
 #portfolio {
-	display: block;
-	color: red;
+  display: flex;
 }
 </style>
